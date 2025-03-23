1bc41a0fb70e0be36344ecc9c4e9ae66f35c0554
threemap <2945853209@qq.com>
2025-03-24 00:30:08 +0800

    更新github

--- config/webgl-examples.js
+++ config/webgl-examples.js
@@ -11,7 +11,27 @@ export default [
                 codeUrl: HOST + '/webglExamples/base/Triangle.html',
                 image: HOST + '/webglExamples/base/Triangle.png', 
             },
-            
+            {
+                id: 'rectangle',
+                name: '两个三角形绘制矩形',
+                author: 'giser2017',
+                codeUrl: HOST + '/webglExamples/base/rectangle.html',
+                image: HOST + '/webglExamples/base/rectangle.png', 
+            },
+            {
+                id: 'IndexDrawingRectangle',
+                name: '索引绘制矩形',
+                author: 'giser2017',
+                codeUrl: HOST + '/webglExamples/base/IndexDrawingRectangle.html',
+                image: HOST + '/webglExamples/base/IndexDrawingRectangle.png', 
+            }, 
+            {
+                id: 'Transformation',
+                name: '变换',
+                author: 'giser2017',
+                codeUrl: HOST + '/webglExamples/base/Transformation.html',
+                image: HOST + '/webglExamples/base/Transformation.png', 
+            }
         ]
     },
     
